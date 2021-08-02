6f6763631545210e1c3dd38520eee4a38e9af688
Thomas Vantuycom <thomasvantuycom@protonmail.com>
2021-08-02 19:45:38 +0200

    style: format according to new XO rules

--- index.js
+++ index.js
@@ -6,119 +6,122 @@ const cloudinary = require('cloudinary').v2;
 const Vinyl = require('vinyl');
 const vinylFile = require('vinyl-file');
 
-module.exports = (options) => {
-  if (!process.env.CLOUDINARY_URL) {
-    if (!options.config) {
-      throw new PluginError(
-        'gulp-cloudinary-upload',
-        'Missing cloudinary config',
-        {showProperties: false}
-      );
-    }
+module.exports = options => {
+	if (!process.env.CLOUDINARY_URL) {
+		if (!options.config) {
+			throw new PluginError(
+				'gulp-cloudinary-upload',
+				'Missing cloudinary config',
+				{showProperties: false}
+			);
+		}
 
-    cloudinary.config(options.config);
-  }
+		cloudinary.config(options.config);
+	}
 
-  return through.obj((file, enc, cb) => {
-    const uploadParameters = {
-      overwrite: false,
-      filename: file.stem,
-      use_filename: true,
-      unique_filename: false,
-      ...options.params
-    };
+	return through.obj((file, enc, cb) => {
+		const uploadParameters = {
+			overwrite: false,
+			filename: file.stem,
+			use_filename: true,
+			unique_filename: false,
+			...options.params
+		};
 
-    if (file.isNull()) {
-      cb(null, file);
-      return;
-    }
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
 
-    if (file.isBuffer()) {
-      cloudinary.uploader
-        .upload_stream(uploadParameters, (error, result) => {
-          if (error) {
-            return cb(new PluginError('gulp-cloudinary-upload', error.message));
-          }
+		if (file.isBuffer()) {
+			cloudinary.uploader
+				.upload_stream(uploadParameters, (error, result) => {
+					if (error) {
+						return cb(new PluginError('gulp-cloudinary-upload', error.message));
+					}
 
-          file.cloudinary = result;
-          return cb(null, file);
-        })
-        .end(file.contents);
-    }
+					file.cloudinary = result;
+					return cb(null, file);
+				})
+				.end(file.contents);
+		}
 
-    if (file.isStream()) {
-      file.contents.pipe(
-        cloudinary.uploader.upload_stream(uploadParameters, (error, result) => {
-          if (error) {
-            return cb(new PluginError('gulp-cloudinary-upload', error.message));
-          }
+		if (file.isStream()) {
+			file.contents.pipe(
+				cloudinary.uploader.upload_stream(uploadParameters, (error, result) => {
+					if (error) {
+						return cb(new PluginError('gulp-cloudinary-upload', error.message));
+					}
 
-          file.cloudinary = result;
-          return cb(null, file);
-        })
-      );
-    }
-  });
+					file.cloudinary = result;
+					return cb(null, file);
+				})
+			);
+		}
+	});
 };
 
-const getManifestFile = (options) =>
-  vinylFile.read(options.path, options).catch((error) => {
-    if (error.code === 'ENOENT') {
-      return new Vinyl(options);
-    }
+const getManifestFile = async options => {
+	try {
+		return await vinylFile.read(options.path, options);
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			return new Vinyl(options);
+		}
 
-    throw error;
-  });
+		throw error;
+	}
+};
 
-module.exports.manifest = (options) => {
-  options = {
-    path: 'cloudinary-manifest.json',
-    merge: false,
-    ...options
-  };
-  options.base = path.dirname(options.path);
+module.exports.manifest = options => {
+	options = {
+		path: 'cloudinary-manifest.json',
+		merge: false,
+		...options
+	};
+	options.base = path.dirname(options.path);
 
-  let manifest = {};
+	let manifest = {};
 
-  return through.obj(
-    (file, enc, cb) => {
-      if (!file.cloudinary) {
-        return cb();
-      }
+	return through.obj(
+		(file, enc, cb) => {
+			if (!file.cloudinary) {
+				return cb();
+			}
 
-      const basename = path.basename(file.path);
-      manifest[basename] = file.cloudinary;
-      cb();
-    },
-    function (cb) {
-      if (Object.keys(manifest).length === 0) {
-        cb();
-        return;
-      }
+			const basename = path.basename(file.path);
+			manifest[basename] = file.cloudinary;
+			cb();
+		},
+		function (cb) {
+			if (Object.keys(manifest).length === 0) {
+				cb();
+				return;
+			}
 
-      (async () => {
-        try {
-          const manifestFile = await getManifestFile(options);
+			(async () => {
+				try {
+					const manifestFile = await getManifestFile(options);
 
-          if (options.merge && !manifestFile.isNull()) {
-            let oldManifest = {};
+					if (options.merge && !manifestFile.isNull()) {
+						let oldManifest = {};
 
-            try {
-              oldManifest = JSON.parse(manifestFile.contents.toString());
-            } catch {}
+						try {
+							oldManifest = JSON.parse(manifestFile.contents.toString());
+						} catch {}
 
-            manifest = Object.assign(oldManifest, manifest);
-          }
+						manifest = Object.assign(oldManifest, manifest);
+					}
 
-          manifestFile.contents = Buffer.from(
-            JSON.stringify(manifest, null, 2)
-          );
-          this.push(manifestFile);
-          cb();
-        } catch (error) {
-          cb(error);
-        }
-      })();
-    }
-  );
+					manifestFile.contents = Buffer.from(
+						JSON.stringify(manifest, null, 2)
+					);
+					this.push(manifestFile);
+					cb();
+				} catch (error) {
+					cb(error);
+				}
+			})();
+		}
+	);
 };
